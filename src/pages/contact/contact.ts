f0a762b41db2254782b22f28ea8b2cdc4de55980
AllanV <a.vanbleus@gmail.com>
2018-02-15 13:17:16 +0100

    Implémentation modals détail et page contact

--- src/pages/contact/contact.ts
+++ src/pages/contact/contact.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 import { NavController } from 'ionic-angular';
 
 @Component({
@@ -6,9 +7,25 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'contact.html'
 })
 export class ContactPage {
+  private contact : FormGroup;
 
-  constructor(public navCtrl: NavController) {
+  private validFormMessage: string;
 
+  constructor(public navCtrl: NavController, private formBuilder: FormBuilder) {
+    this.contact = this.formBuilder.group({
+      nom: ["", Validators.required],
+      prenom: ["", Validators.required],
+      mail: ["", Validators.required],
+      message: ["", Validators.required]
+    });
+    this.validFormMessage = "";
   }
 
+  contactAssociation() {
+    console.log("Association contacted", this.contact.value);
+    this.validFormMessage = "Message envoyé !";
+    setTimeout(() => {
+      this.validFormMessage = "";
+    }, 2000);
+  }
 }
